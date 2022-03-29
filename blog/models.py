5dc374daa0f7fe6f2b9f6072b1fa51ef3ccac410
Anvar Iskhakov <anvartar2002@gmail.com>
2022-03-29 15:50:14 +0300

    Added specialization, workspace and related models

--- blog/models.py
+++ blog/models.py
@@ -49,3 +49,23 @@ class Comment(models.Model):
     
     def __str__(self):
         return f'Comment {self.id} by {self.author.username}'
+
+    
+class Specialization(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class user_specialization(models.Model):
+    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    years_of_experience = models.PositiveSmallIntegerField(null=True, blank=True)
+
+
+class Workspace(models.Model):
+    name = models.CharField(max_length=500)
+
+
+class user_workspace(models.Model):
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    years_working = models.PositiveSmallIntegerField(null=True, blank=True)
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+
+from blog.models import Specialization, Workspace, user_specialization, user_workspace
 from .models import Feedback, User, Profile
 
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Feedback)
+admin.site.register(Specialization)
+admin.site.register(user_specialization)
+admin.site.register(Workspace)
+admin.site.register(user_workspace)
